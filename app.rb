96860b549f0ca080a927936d7725ac93de57a2c9
Brian Waddell <139478254+Brian-Waddell@users.noreply.github.com>
2023-09-15 20:43:49 +0000

    changes

--- app.rb
+++ app.rb
@@ -14,9 +14,16 @@ get("/square/new") do
 end 
 
 get ("/square/results") do 
- 
-  @the_num = params.fetch("user_number").to_f
-
+  @the_num = params.fetch("users_number").to_f
   @the_result = @the_num ** 2 
   erb(:square_results)
 end
+
+get("square_root/new") do 
+
+  erb(:square_root_new)
+end 
+get("/square_root/results") do 
+
+
+  erb(:square_root_results)
